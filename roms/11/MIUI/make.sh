0f2431c6249ea5adb9981ebf657ddb75bf495b3d
JamieHoSzeYui <henloboii@protonmail.ch>
2021-04-19 08:42:02 +0800

    roms: MIUI: Misc improvements
    * NQNfcNci moved to ext
    * MIUI still uses props to determine FOD (yes)
    * device features will still be needed for round corners

--- roms/11/MIUI/make.sh
+++ roms/11/MIUI/make.sh
@@ -5,3 +5,14 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # Disable setup wizard
 echo "DEVICE_PROVISIONED=1" >> $1/build.prop
+
+# FOD 
+echo "ro.hardware.fp.fod=true" >> $1/build.prop
+echo "persist.vendor.sys.fp.fod.location.X_Y=445,1260" >> $1/build.prop
+echo "persist.vendor.sys.fp.fod.size.width_height=190,190" >> $1/build.prop
+
+# drop nfc
+rm -rf $1/system_ext/app/NQNfcNci
+
+# round cockners
+sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
